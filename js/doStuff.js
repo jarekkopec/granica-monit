b9cdf3aba5289ef159f6ef175ea1c089dadbbef3
bekonowicz <jaroslaw.kopec@gmail.com>
2022-03-06 20:51:59 +0100

    ukrainian side of the border operational

--- js/doStuff.js
+++ js/doStuff.js
@@ -2,17 +2,15 @@ google.charts.load('current');
 google.charts.setOnLoadCallback(init);
 
 
-function composeLabel(d) {
-    console.log(d.wolontariusze);
-    let labelConent = '';
-    if (d.auta_kierowcy == 1) {
-        labelConent = '<tspan class="iconGrey">\ue84d</tspan>'
-    } else if (d.auta_kierowcy == 2) {
-        labelConent = '<tspan class="iconYellow">\ue84d</tspan>'
-    } else if (d.auta_kierowcy == 3) {
-        labelConent = '<tspan class="iconGrey">\ue84d</tspan>'
+function composeLabelPolishSide(d) {
+    let labelConent = '<tspan class="labelText">' + d.place_name + '</tspan>&nbsp;';
+    if (d.autaKierowcy == 1) {
+        labelConent += '<tspan class="iconGrey">\ue84d</tspan>'
+    } else if (d.autaKierowcy == 2) {
+        labelConent += '<tspan class="iconYellow">\ue84d</tspan>'
+    } else if (d.autaKierowcy == 3) {
+        labelConent += '<tspan class="iconGrey">\ue84d</tspan>'
     }
-
 
     if (d.wolontariusze == 1) {
         labelConent += '<tspan class="iconGrey">&nbsp;\ue8a5</tspan>'
@@ -33,12 +31,23 @@ function composeLabel(d) {
     return labelConent;
 }
 
+function composeLabelUkrainianSide(d) {
+    let labelConent = '';
+    if (d.zapchanieOgolne == 1) {
+        labelConent += '<tspan class="iconGrey">\ue82b</tspan>'
+    } else if (d.zapchanieOgolne == 2) {
+        labelConent += '<tspan class="iconYellow">\ue82b</tspan>'
+    } else if (d.zapchanieOgolne == 3) {
+        labelConent += '<tspan class="iconGrey">\ue82b</tspan>'
+    }
+    
+    return labelConent;
+}
 
 function renderMap(array) {
 
     d3.json('js/woj.geojson')
     .then(function(data) {
-        console.log(data);
         
         let projection = d3.geoMercator();
         projection.fitExtent([[0, 0], [350, 400]], data);
@@ -71,14 +80,33 @@ function renderMap(array) {
             .enter().append('g')
             .classed('label', true);
         
-        let labelName = labels.append('text').text(function(d) { return d.place_name; })    
+        let labelPolishSide = labels.append('text').html(function(d) { return composeLabelPolishSide(d); })    
             .attr('x', function(d) { return projection([d.lon, d.lat])[0] - 10; })
             .attr('y', function(d) { return projection([d.lon, d.lat])[1] + 3; })
             .attr('text-anchor', 'end');
         
-        let labelIcons = labels.append('text').html(function(d) { return composeLabel(d); })
+        let labelUkrainianSide = labels.append('text').html(function(d) { return composeLabelUkrainianSide(d); })
             .attr('x', function(d) { return projection([d.lon, d.lat])[0] + 15; })
             .attr('y', function(d) { return projection([d.lon, d.lat])[1] + 3; });
+        
+        let updateDatesArray = array;
+        
+        let updateDatesPoland = [];
+        let updateDatesUkraine = [];
+        
+        for (i=0; i<array.length; i++) {
+            updateDatesPoland.push(array[i].updatePolska);
+            updateDatesUkraine.push(array[i].updateUkraina);
+        }
+
+        console.log(updateDatesPoland);
+        console.log(updateDatesUkraine);
+        
+        // let updateData = d3.select('#updateDates')
+        //     .append('p')
+        //     .text(function(d) {
+
+        //     })
         
 
     })
@@ -90,8 +118,6 @@ function processSheetsData(response) {
     var data = response.getDataTable();
     var numberOfRows = data.getNumberOfRows();
     var numberOfColumns = data.getNumberOfColumns();
-    console.log(numberOfRows);
-    console.log(numberOfColumns);
     for (r=1; r<numberOfRows; r++) {
         console.log(data.getFormattedValue(r, 0))
         row = {
@@ -99,10 +125,14 @@ function processSheetsData(response) {
             lat: data.getFormattedValue(r, 1),
             lon: data.getFormattedValue(r, 2),
             dojazd: data.getFormattedValue(r, 3),
-            auta_kierowcy: data.getFormattedValue(r, 4),
+            autaKierowcy: data.getFormattedValue(r, 4),
+            potrzebyMedyczne: data.getFormattedValue(r, 5),
             wolontariusze: data.getFormattedValue(r, 6),
             magazyny: data.getFormattedValue(r, 7),
-            
+            zapchanieOgolne: data.getFormattedValue(r, 8),
+            updateUkraina: data.getFormattedValue(r, 11),
+            updatePolska: data.getFormattedValue(r, 12)
+
         };
         
         array.push(row);
@@ -114,6 +144,6 @@ function processSheetsData(response) {
 function init() {
   var url = 'https://docs.google.com/spreadsheets/d/1id8a5Hp8OCk9PSgoCY4b9mSOCZLJ_YgUW1bQHmaUGPI/edit?usp=sharing';
   var query = new google.visualization.Query(url);
-  query.setQuery('select A, B, C, D, E, F, G, H, I, J, K');
+  query.setQuery('select A, B, C, D, E, F, G, H, I, J, K, L, M');
   query.send(processSheetsData);
 }
